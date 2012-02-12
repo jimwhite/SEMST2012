d0b8d13173932bb9ff1af4ad88b50517446aad92
Jim White <james.paul.white@gmail.com>
2012-02-12 06:05:20 -0800

    path fix

--- parse.cmd
+++ parse.cmd
@@ -1,7 +1,8 @@
+_source = /home2/jimwhite/Projects/gondor.school/SEMST2012/data/dev50_sentences.txt
 Universe   = vanilla
-Environment=LOGONROOT=~/Projects/logon;PATH=/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/opt/git/bin:/opt/scripts:/condor/bin
+Environment = LOGONROOT=/home2/jimwhite/Projects/logon;PATH=/usr/kerberos/bin:/usr/local/bin:/bin:/usr/bin:/opt/git/bin:/opt/scripts:/condor/bin
 Executable  = /home2/jimwhite/Projects/gondor.school/SEMST2012/parse.sh
-Arguments   = --binary --erg+tnt -best 1 --export --text /home2/jimwhite/Projects/gondor.school/SEMST2012/data/dev50_sentences.txt
+Arguments   = --binary --erg+tnt -best 1 --export --text $(_source)
 Log         = parse.log
 Output 		= parse.out
 Error	    = parse.err
